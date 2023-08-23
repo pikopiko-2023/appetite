8e28426a75356b6336c0e380fc0c61bdc3d9d43a
Jayde Medder <jayde.medder@hotmail.com>
2023-08-23 12:13:22 +1200

    Edited migration and seeds for users

--- server/db/migrations/20160802144759_users.js
+++ server/db/migrations/20160802144759_users.js
@@ -2,6 +2,7 @@ export async function up(knex) {
   await knex.schema.createTable('users', (table) => {
     table.increments('id').primary()
     table.string('name')
+    table.string('about')
     table.string('email')
   })
 }
--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -1,38 +1,25 @@
 export async function seed(knex) {
   await knex('users').insert([
-    { id: 99901, name: 'Ambitious Aardvark', email: 'aardvark@example.org' },
-    { id: 99902, name: 'Bamboozled Baboon', email: 'baboon@example.org' },
-    { id: 99903, name: 'Curious Capybara', email: 'capybara@example.org' },
-    { id: 99904, name: 'Dilapidated Duck', email: 'duck@example.org' },
-    { id: 99905, name: 'Exuberant Elephant', email: 'elephant@example.org' },
     {
-      id: 99906,
-      name: 'Fascinated Flying Fox',
-      email: 'flying.fox@example.org',
+      id: 99901,
+      name: 'Anton Ego',
+      about:
+        "Anton Ego is a cutthroat food critic who was made infamous for his scathing reviews and cold exterior. Despite his generally negative criticism, Ego is the most acclaimed food connoisseur in Paris. Ego is extremely passionate about food, claiming, \"I don't like food, I love it\", and claims to refuse to swallow food that he doesn't like, which explains why he's so thin.",
+      email: 'anton.ego@users.net',
     },
     {
-      id: 99907,
-      name: 'Generous Gila Monster',
-      email: 'gila.monster@example.org',
+      id: 99902,
+      name: 'Gordon Ramsay',
+      about:
+        "Gordon Ramsey is a British celebrity chef, restaurateur, television presenter, and writer. His restaurant group, Gordon Ramsay Restaurants, was founded in 1997 and has been awarded 17 Michelin stars overall; it currently holds a total of seven. Gordon's reviews are so brutally honest, even the vegetables start crying in the kitchen!",
+      email: 'gordo@users.net',
     },
-    { id: 99908, name: 'Hilarious Heron', email: 'heron@example.org' },
-    { id: 99909, name: 'Intransigent Impala', email: 'impala@example.org' },
-    { id: 99910, name: 'Jocular Jerboa', email: 'jerboa@example.org' },
-    { id: 99911, name: 'Kafkaesque Kinkajou', email: 'kinkajou@example.org' },
-    { id: 99912, name: 'Loquacious Lemur', email: 'lemur@example.org' },
-    { id: 99913, name: 'Misanthropic Mongoose', email: 'mongoose@example.org' },
-    { id: 99914, name: 'Nonchalant Nyala', email: 'nyala@example.org' },
-    { id: 99915, name: 'Ornery Ocelot', email: 'ocelot@example.org' },
-    { id: 99916, name: 'Peaceful Pangolin', email: 'panda@example.org' },
-    { id: 99917, name: 'Querulous Quokka', email: 'quokka@example.org' },
-    { id: 99918, name: 'Resolute Rail', email: 'rail@example.org' },
-    { id: 99919, name: 'Senescent Sloth', email: 'sloth@example.org' },
-    { id: 99920, name: 'Tumultuous Terrapin', email: 'terrapin@example.org' },
-    { id: 99921, name: 'Unassailable Urial', email: 'urial@example.org' },
-    { id: 99922, name: 'Voracious Viscacha', email: 'viscacha@example.org' },
-    { id: 99923, name: 'Wondering Wombat', email: 'wombat@example.org' },
-    { id: 99924, name: 'Xenial Xerus', email: 'xerus@example.org' },
-    { id: 99925, name: 'Yielding Yak', email: 'yak@example.org' },
-    { id: 99926, name: 'Zaftig Zebu', email: 'zebu@example.org' },
+    {
+      id: 99903,
+      name: 'Po',
+      about:
+        "Po's favourite food is noodles, and he's known for his huge appetite. Before becoming the Dragon Warrior, Po worked at his father's noodle shop and dreams of being a kung fu master. \"Imagine a dish that's the Picasso of the culinary world – each bite a delightful surprise, flavours doing the cha-cha, and shrimp that moonwalk across your plate. 'Flavour Fusion Fantasia' isn't just a meal; it's a front-row ticket to a culinary circus!",
+      email: 'popopo@users.net',
+    },
   ])
 }
