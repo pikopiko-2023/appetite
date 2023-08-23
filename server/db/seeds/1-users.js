6680451951e66eca03934356fac2219450c27d5e
Jayde Medder <jayde.medder@hotmail.com>
2023-08-23 16:44:37 +1200

    migration edits

--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -6,6 +6,7 @@ export async function seed(knex) {
       about:
         "Anton Ego is a cutthroat food critic who was made infamous for his scathing reviews and cold exterior. Despite his generally negative criticism, Ego is the most acclaimed food connoisseur in Paris. Ego is extremely passionate about food, claiming, \"I don't like food, I love it\", and claims to refuse to swallow food that he doesn't like, which explains why he's so thin.",
       email: 'anton.ego@users.net',
+      img: '/images/fake-user/Anton_Ego.png',
     },
     {
       id: 99902,
@@ -13,6 +14,7 @@ export async function seed(knex) {
       about:
         "Gordon Ramsey is a British celebrity chef, restaurateur, television presenter, and writer. His restaurant group, Gordon Ramsay Restaurants, was founded in 1997 and has been awarded 17 Michelin stars overall; it currently holds a total of seven. Gordon's reviews are so brutally honest, even the vegetables start crying in the kitchen!",
       email: 'gordo@users.net',
+      img: '/images/fake-user/Gordon_Ramsey.png',
     },
     {
       id: 99903,
@@ -20,6 +22,7 @@ export async function seed(knex) {
       about:
         "Po's favourite food is noodles, and he's known for his huge appetite. Before becoming the Dragon Warrior, Po worked at his father's noodle shop and dreams of being a kung fu master. \"Imagine a dish that's the Picasso of the culinary world – each bite a delightful surprise, flavours doing the cha-cha, and shrimp that moonwalk across your plate. 'Flavour Fusion Fantasia' isn't just a meal; it's a front-row ticket to a culinary circus!",
       email: 'popopo@users.net',
+      img: '/images/fake-user/Po.png',
     },
   ])
 }
