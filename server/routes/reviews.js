a1212f2855c0b8d0906cb2c716a35e709c48f111
Suwon Jeong <integer.won@gmail.com>
2023-08-23 23:42:40 +1200

    Add db, route and hbs for adding new review

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -9,11 +9,71 @@ router.get('/:id/reviews', async (req, res) => {
   try {
     const id = Number(req.params.id)
     const reviews = await db.getReviews(id)
-    console.log(reviews)
-    res.render('review', { reviews })
+    const restaurant = await db.getRestaurant(id)
+
+    const viewReviews = {
+      restaurant: restaurant,
+      reviews: reviews,
+    }
+
+    res.render('review', { viewReviews })
   } catch (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   }
 })
+
+// /restaurants/77702/addReview
+router.get('/:id/addReview', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const restaurant = await db.getRestaurant(id)
+    res.render('addReview', { restaurant: restaurant })
+  } catch (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  }
+})
+
+// /restaurants/77702/addReview
+router.post('/:id/addReview', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const { name, email, rating, review } = req.body
+    const allReviews = await db.getAllReviews()
+    const userId = await findUserId(name)
+
+    const newReview = {
+      id: allReviews.length + 88801,
+      rating: rating,
+      review: review,
+      user_id: userId,
+      restaurant_id: id,
+    }
+
+    const newUser = {
+      id: userId,
+      name: name,
+      about: 'New reviewer',
+      email: email,
+      img: '/images/fake-user/new_user.png',
+    }
+
+    await db.addNewReview(newReview)
+    await db.addUser(newUser)
+    res.redirect(`reviews`)
+  } catch (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  }
+})
+
+async function findUserId(name) {
+  const users = await db.getUsers()
+  const user = users.find((user) => user.name === name)
+
+  if (user) {
+    return user.id
+  } else {
+    return users.length + 99901
+  }
+}
 
 export default router
